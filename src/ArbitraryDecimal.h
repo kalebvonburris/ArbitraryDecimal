73d2fa48d477cab0b71cece31738dc72459a2058
Kaleb Burris <kaleb.r.burris@gmail.com>
2023-04-12 19:07:58 -0800

    Update

--- src/ArbitraryDecimal.h
+++ src/ArbitraryDecimal.h
@@ -1,6 +1,8 @@
 #include <vector>
 #include <limits>
 #include <string>
+#include <sstream>
+#include <numeric>
 
 class ArbitraryDecimal {
 
@@ -14,7 +16,6 @@ private:
     // Utility values
     // Sign will be 1 if the value is positive and -1 if the value is negative.
     int sign;
-    int separator_index;
 
     // Constants
     int const numerator_max = INT_MAX;
@@ -24,11 +25,11 @@ public:
     // Getters
     std::vector<int> const getNumerators();
     std::vector<int> const getDenominators();
-    int const getSeparatorIndex();
     int const getSign();
 
     // Constructors
     ArbitraryDecimal();
+    ArbitraryDecimal(std::vector<int> n, std::vector<int> d, int sign);
     ArbitraryDecimal(std::string value);
     ArbitraryDecimal(int);
     ArbitraryDecimal(long);
@@ -45,26 +46,50 @@ public:
     ArbitraryDecimal divide   (ArbitraryDecimal other);
     ArbitraryDecimal operator/(ArbitraryDecimal other);
     ArbitraryDecimal pow      (ArbitraryDecimal other);
-    ArbitraryDecimal operator^(ArbitraryDecimal other);
+    ArbitraryDecimal operator^(ArbitraryDecimal other);\
+
+    // Utility functions
+    void simplifyFraction(int &numerator, int &denominator);
 
     // Conversions
     int const asInt();
     long const asLong();
     double const asDouble();
     float const asFloat();
-    std::string const asString();
+    std::string const asString(int precision);
 };
 
 ArbitraryDecimal::ArbitraryDecimal()  :
 numerators{(0, 0, 0)}, 
 denominators{(1, 0, 1)},
-sign{1}, 
-separator_index{1}
+sign{1}
 {
     // This space left intentionally blank.
 }
 
+ArbitraryDecimal::ArbitraryDecimal(std::vector<int> n, std::vector<int> d, int sign) {
+    numerators = n;
+    denominators = d;
+    this->sign = sign;
+}
+
+
 std::vector<int> const ArbitraryDecimal::getDenominators() {return denominators;}
 std::vector<int> const ArbitraryDecimal::getNumerators() {return numerators;}
-int const ArbitraryDecimal::getSeparatorIndex() {return separator_index;}
 int const ArbitraryDecimal::getSign() {return sign;}
+
+void ArbitraryDecimal::simplifyFraction(int &numerator, int &denominator) {
+    int gcd = std::gcd(numerator, denominator);
+    if (gcd != 1) {
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+    return;
+}
+
+// std::string const ArbitraryDecimal::asString(int precision) {
+//     std::stringstream ss;
+//     for (int i = precision, index = numerators.size(); i != 0;) {
+//         
+//     }
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,16 @@ using namespace std;
 
 int main() {
 
-    ArbitraryDecimal d{};
+    vector<int> n = {1,0,24};
+    vector<int> d = {1,0,32};
 
-    cout << d.getSign() << endl;
+    ArbitraryDecimal ad{n, d, 1};
+
+    int a = 24;
+    int b = 32;
+
+    ad.simplifyFraction(a, b);
+
+    cout << a << " " << b << endl;
 
 }
